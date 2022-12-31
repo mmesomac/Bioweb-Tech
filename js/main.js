cf8d05bbb2f640cc937539cfcc9a21cedb3645ab
Mmesoma <nmmeso.c@gmail.com>
2022-12-31 04:29:21 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc,
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+(function () {
+  'use strict';
+
+  var carousels = function () {
+    $('.owl-carousel1').owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+        },
+      },
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
